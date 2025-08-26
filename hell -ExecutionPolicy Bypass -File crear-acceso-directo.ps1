32c58ab0a89c1722af86bfc9f88954004efb814a
turispuebla-beep <turispuebla@gmail.com>
2025-08-26 22:42:39 +0200

    CDSANABRIACF - Versión Final Limpia y Optimizada

---
+++ hell -ExecutionPolicy Bypass -File crear-acceso-directo.ps1
@@ -0,0 +1,5 @@
+[33m03594c4[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m  CDSANABRIACF - Versión Completa y Finalizada
+[33m75b364a[m README actualizado con todas las mejoras implementadas
+[33md33d3ff[m Aplicadas todas las mejoras: sincronización en tiempo real, lenguaje inclusivo y contraseñas
+[33mc4a5dc0[m Aplicadas todas las mejoras: sincronización en tiempo real, lenguaje inclusivo y contraseñas
+[33m2bbbdf5[m README actualizado con sincronización en tiempo real y lenguaje inclusivo
